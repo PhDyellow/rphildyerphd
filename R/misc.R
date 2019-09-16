0d00bb2db35e621d7426f0ab7b6c0db74ffc79b1
Phil Dyer <phildyer@protonmail.com>
2019-09-17 09:22:12 +1000

    Fix missing full.names

--- R/misc.R
+++ R/misc.R
@@ -8,6 +8,6 @@
 #' @param cache_env an env that has already been created to capture the objects
 #' @param ext cache file extension
 load_cache_dir <- function(path, cache_env, ext = "RData"){
-  files <- do.call(list.files, list(path = path, pattern = paste0("\\.", ext, "$"), full.names = full.names))
+  files <- do.call(list.files, list(path = path, pattern = paste0("\\.", ext, "$"), full.names = TRUE))
   lapply(files, load, cache_env)
 }
